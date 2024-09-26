d36e2f32491ac1bf76ae69676d0a6e55496a86a7
YanMo <xiajiaruimail@qq.com>
2024-09-26 21:47:27 +0800

    修改 logo

--- src/main.js
+++ src/main.js
@@ -17,6 +17,6 @@ app.mount("#app");
 // PWA
 navigator.serviceWorker.addEventListener("controllerchange", () => {
   // 弹出更新提醒
-  console.log("站点已更新，刷新后生效");
+  console.log("Website updated, please refresh");
   ElMessage("站点已更新，刷新后生效");
 });
